3de594fe276fbe3e31a2f55ecdc580c7462f9393
Taharka200 <sammbasow1999@gmail.com>
2024-06-26 16:23:56 +0000

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,195 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+    <>
+      <header>
+        <a href="#" className="logo-holder">
+          <div className="logo">L</div>
+          <div className="logo-text">Portfolio Website</div>
+        </a>
+        <nav>
+          <ul id="menu" className={menuOpen ? "active" : ""}>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="mailto:sammbasow1999@gmail.com">Contact me</a>
+            </li>
+          </ul>
+          <a href="#" className="mobile-toggle" onClick={toggleMobileMenu} >
+            <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h10" />
+            </svg>
           </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+        </nav>
+      </header>
+      <main>
+        <section className="hero container">
+          <div className="hero-blue">
+            <div>
+              <h1><small>Hi I'm</small> Sammba Yero Taharka Sow</h1>
+              <p>
+                Software developer in Senegal . <span></span>
+              </p>
+              <div className="call-to-action">
+                <a href="Cv.pdf" className="button black">View Resume</a>
+                <a href="mailto:sammbasow1999@gmail.com" className="button white">Contact Me</a>
+              </div>
+              <div className="social-links">
+                <a href="https://github.com/Taharka2000?tab=repositories">
+                  <img src="images/github.png" alt="GitHub" width="48" />
+                </a>
+                <a href="#">
+                  <img src="images/linkdin.png" alt="LinkedIn" width="48" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="hero-yellow">
+            <img src="images/sammba-removebg-preview.png" alt="Sammba Sow" width="100%" />
+          </div>
+        </section>
+        <section className="container logos">
+          <div className="marquee">
+            <div className="track">
+              <img src="images/react.png" alt="React" />
+              <img src="images/angular.png" alt="Angular" />
+              <img src="images/mongodb.png" alt="MongoDB" />
+              <img src="images/tailwind.png" alt="Tailwind" />
+              <img src="images/node.png" alt="Nodejs" />
+              <img src="images/ts.png" alt="Tailwind" />
+              <img src="images/css.png" alt="Tailwind" />
+              <img src="images/html.png" alt="Tailwind" />
+              <img src="images/word.png" alt="Tailwind" />
+              <img src="images/sql.png" alt="Tailwind" />
+              <img src="images/express.png" alt="Tailwind" />
+            </div>
+          </div>
+        </section>
+        <section id="skills" className="skills container">
+          <h2>
+            <small>About Me</small>
+            Skills
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <div className="holder-blue">
+            <div className="left-column">
+              <h3>FrontEnd</h3>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>Angular</li>
+                <li>React</li>
+                <li>Tailwind</li>
+              </ul>
+              <h3>BackEnd</h3>
+              <ul>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>SpringBoot</li>
+              </ul>
+              <h3>CMS</h3>
+              <ul>
+                <li>WordPress</li>
+              </ul>
+              <h3>Base de Donne</h3>
+              <ul>
+                <li>Sql</li>
+                <li>NoSql</li>
+              </ul>
+            </div>
+            <div className="right-column">
+              <h3>A bit About Me</h3>
+              <p> Développeur junior passionné avec une solide formation académique en informatique et une expérience pratique dans le développement de logiciels. et ayant une bonne compréhension des concepts de développement web et mobile. Capable de travailler efficacement en équipe ou de manière autonome, avec un fort désir d'apprendre et de s'adapter aux nouvelles technologies. Engagé à fournir des solutions de haute qualité et à contribuer activement aux projets de développement.?</p>
+              <p></p>
+            </div>
+          </div>
+        </section>
+        <section className="work-experience container">
+          <h2>
+            <small>Recent</small>
+            Work Experience
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <div className="jobs">
+            <article>
+              <figure>
+                <div>
+                  <img src="images/image.png" alt="Workplace-1" width="100%" />
+                  <figcaption>Instructeur Web </figcaption>
+                </div>
+              </figure>
+              <h3>Instructeur online en software developpeur en Part Time</h3>
+              <div>2024-Now</div>
+              <p>Formation et Accompagnement des étudiants à
+                travers des projets pratiques, fournissant un soutien
+                technique personnalisé pour initier ou améliorer leurs
+                compétences en codage.</p>
+            </article>
+            {/*  <article>
+              <figure>
+                <div>
+                  <img src="images/image.png" alt="Workplace-1" width="100%" />
+                  <figcaption>Workplace-1</figcaption>
+                </div>
+              </figure>
+              <h3>Workplace -1</h3>
+              <div>2024-Now</div>
+              <p>Instructor Web</p>
+            </article>
+            <article>
+              <figure>
+                <div>
+                  <img src="images/image.png" alt="Workplace-1" width="100%" />
+                  <figcaption>Workplace-1</figcaption>
+                </div>
+              </figure>
+              <h3>Workplace -1</h3>
+              <div>2024-Now</div>
+              <p>Instructor Web</p>
+            </article> */}
+          </div>
+        </section>
+        <section id="projects" className="bento container">
+          <h2>
+            <small>Previous</small>
+            Completed Projects
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <div className="bento-grid">
+            <a href="https://github.com/Taharka2000/React-Router" className="bento-item">
+              <img src="images/exp2.png" alt="Workplace-1" width="100%" />
+            </a>
+            <a href="#" className="bento-item">
+              <img src="images/exp3.png" alt="Workplace-1" width="100%" />
+            </a>
+            <a href="#" className="bento-item">
+              <img src="images/exp3.png" alt="Workplace-1" width="100%" />
+            </a>
+            <a href="#" className="bento-item">
+              <img src="images/exp3.png" alt="Workplace-1" width="100%" />
+            </a>
+            <a href="#" className="bento-item">
+              <img src="images/exp3.png" alt="Workplace-1" width="100%" />
+            </a>
+            <a href="https://github.com/Taharka2000/FrontEnd-ProjetFinal" className="bento-item">
+              <img src="images/exp1.png" alt="Workplace-1" width="100%" />
+            </a>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
