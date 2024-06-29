41ee8955d5576aef5fb498a6ce079a6cefb3130b
Taharka200 <sammbasow1999@gmail.com>
2024-06-29 19:20:43 +0000

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable @next/next/no-page-custom-font */
-// RootLayout.tsx
-import { Fragment } from 'react';
-import Head from 'next/head';
-import './globals.css';
+
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Website Portfolio',
-  description: 'Portfolio website design',
+  title: "Website Portfolio",
+  description: "Portfolio website design",
 };
 
 export default function RootLayout({
@@ -15,28 +14,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Fragment>
-      <Head>
+    <html lang="en">
+      <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
           rel="stylesheet"
         />
-      </Head>
-      <body>
-        <html lang="en">
-          {children}
-        </html>
-      </body>
-    </Fragment>
+      </head>
+      <body>{children}</body>
+    </html>
   );
 }
